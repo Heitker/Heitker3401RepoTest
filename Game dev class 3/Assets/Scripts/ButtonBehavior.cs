d106aeed565bfdfd2c485c15aafc8aac279d2764
Heitker <mh011216@ohio.edu>
2020-02-10 16:45:12 -0500

    In class work from week 5

---
+++ Game dev class 3/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public AudioSource buttonAudioSource;
+
+    public void OnButtonClick ()
+    {
+        if (buttonAudioSource.isPlaying)
+        {
+            buttonAudioSource.Stop();
+        }
+        else
+        {
+            buttonAudioSource.Play();
+        }
+    }
+}
+
